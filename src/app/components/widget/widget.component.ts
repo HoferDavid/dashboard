5d6053efa1b2e5f064e8dc9b426a44b7dd6ebdc8
David Hofer <chassis.poppets0x@icloud.com>
2024-10-10 16:26:17 +0200

    Revert "Revert "settings options""
    This reverts commit 6af6eebbef906335d8fcac7956822d839f4d4de5.

--- src/app/components/widget/widget.component.ts
+++ src/app/components/widget/widget.component.ts
@@ -1,19 +1,26 @@
 import { Component, input, signal } from '@angular/core';
-import { Widget } from '../../interfaces/widget';
+import { Widget } from '../../interfaces/dashboard';
 import { NgComponentOutlet } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { WidgetOptionsComponent } from "./widget-options/widget-options.component";
 
 @Component({
   selector: 'app-widget',
   standalone: true,
-  imports: [NgComponentOutlet, MatButtonModule, MatIconModule, WidgetOptionsComponent],
+  imports: [
+    NgComponentOutlet,
+    MatButtonModule,
+    MatIconModule,
+    WidgetOptionsComponent,
+    MatIcon,
+  ],
   templateUrl: './widget.component.html',
   styleUrl: './widget.component.scss',
   host: {
-    '[style.grid-area]': '"span " + (data().rows ?? 1) + "/ span " + (data().columns ?? 1)',
-  }
+    '[style.grid-area]':
+      '"span " + (data().rows ?? 1) + "/ span " + (data().columns ?? 1)',
+  },
 })
 export class WidgetComponent {
   data = input.required<Widget>();
